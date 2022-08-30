b54bf02ff0b85857a6273434ee1e60240c994a0f
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-08-30 10:02:42 -0400

    Update shop-itemLookup.lua

--- Contents/mods/pz-shops/media/lua/client/shop-itemLookup.lua
+++ Contents/mods/pz-shops/media/lua/client/shop-itemLookup.lua
@@ -1,7 +1,6 @@
 local itemDictionary = {}
 itemDictionary.categories = {}
-
-itemDictionary.f3cPartition = {} -- first 3 characters
+itemDictionary.partition = {} -- first 3 characters
 
 function getItemDictionary() return itemDictionary end
 
@@ -16,14 +15,14 @@ function itemDictionary.assemble()
 
             local itemType = itemScript:getName()
             local first3Char = string.lower(string.sub(itemType,1,3))
-            local itemModuleType = itemScript:getFullName()
-            local displayCategory = itemScript:getDisplayCategory()
             
+            local displayCategory = itemScript:getDisplayCategory()
             if not itemDictionary.categories[displayCategory] then itemDictionary.categories[displayCategory] = string.lower(displayCategory) end
-
+            
             if first3Char and first3Char ~= "" then
-                if not itemDictionary.f3cPartition[first3Char] then itemDictionary.f3cPartition[first3Char] = {} end
-                itemDictionary.f3cPartition[first3Char][itemModuleType] = true
+                if not itemDictionary.partition[first3Char] then itemDictionary.partition[first3Char] = {} end
+                local itemModuleType = itemScript:getFullName()
+                table.insert(itemDictionary.partition[first3Char], itemModuleType)
             end
 
         end
@@ -32,9 +31,9 @@ end
 Events.OnGameBoot.Add(itemDictionary.assemble)
 
 function findMatchesFromItemDictionary(input)
-    local inputF3Char = string.sub(input,1,3)
-    local partitionMatches = itemDictionary.f3cPartition[inputF3Char]
+    local inputChar = string.lower(string.sub(input,1,3))
+    local partitionMatches = itemDictionary.partition[inputChar]
     if not partitionMatches then return end
-    print("inputF3Char: "..inputF3Char)
-    for type,_ in pairs(partitionMatches) do print(" -- "..type) end
+    print("inputChar: "..inputChar)
+    for _,type in pairs(partitionMatches) do print(" -- "..type) end
 end
