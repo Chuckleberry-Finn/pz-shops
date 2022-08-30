20b944eeba5c5d69790d6ed01f7763a3fbe1aed8
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-08-29 22:26:17 -0400

    Update shop-itemLookup.lua

--- Contents/mods/pz-shops/media/lua/client/shop-itemLookup.lua
+++ Contents/mods/pz-shops/media/lua/client/shop-itemLookup.lua
@@ -1,19 +1,9 @@
---[[
-local crossRefMods = { ["BetterSortCC"]="ItemTweaker_Copy_CC", }
-local activeMods = {}
-local activeModIDs = getActivatedMods()
-for i=1, activeModIDs:size() do
-    local modID = activeModIDs:get(i-1)
-    if crossRefMods[modID] and not activeMods[modID] then
-        require (crossRefMods[modID])
-        activeMods[modID] = true
-    end
-end
---]]
-
 local itemDictionary = {}
 itemDictionary.categories = {}
-itemDictionary.fcPartition = {}
+itemDictionary.modules = {}
+
+itemDictionary.f3cPartition = {} -- first 3 characters
+itemDictionary.imPartition = {} -- item module
 
 function getItemDictionary() return itemDictionary end
 
@@ -23,19 +13,34 @@ function itemDictionary.assemble()
     for i=0, allItems:size()-1 do
         ---@type Item
         local itemScript = allItems:get(i)
-        local itemType = itemScript:getName()
-        local firstChar = string.sub(itemType,1,1)
-        local itemModuleType = itemScript:getFullName()
-        --local itemModule = itemScript:getModuleName()
+        local itemModule = itemScript:getModuleName()
+        if not itemScript:getObsolete() and not itemScript:isHidden() and itemModule ~= "Moveables" then
 
-        local displayCategory = itemScript:getDisplayCategory()
-        itemDictionary.categories[displayCategory] = true
+            local itemType =itemScript:getName()
+            local first3Char = string.sub(itemType,1,3)
 
-        if firstChar and firstChar ~= "" then
-            itemDictionary.fcPartition[firstChar] = {}
-            itemDictionary.fcPartition[firstChar][itemModuleType] = true
-            itemDictionary.fcPartition[firstChar][itemType] = true
+            --local itemModuleType = itemScript:getFullName()
+
+            local displayCategory = itemScript:getDisplayCategory()
+            itemDictionary.categories[displayCategory] = string.lower(displayCategory)
+
+            itemDictionary.modules[itemModule] = string.lower(itemModule)
+            itemDictionary.imPartition[itemType] = itemModule
+
+            if first3Char and first3Char ~= "" then
+                itemDictionary.f3cPartition[first3Char] = itemDictionary.f3cPartition[first3Char] or {}
+                itemDictionary.f3cPartition[first3Char][itemType] = string.lower(itemType)
+            end
+
         end
     end
 end
 Events.OnGameBoot.Add(itemDictionary.assemble)
+
+function findMatchesFromItemDictionary(input)
+    local inputF3Char = string.sub(input,1,3)
+    local partitionMatches = itemDictionary.f3cPartition[inputF3Char]
+    if not partitionMatches then return end
+    print("inputF3Char: "..inputF3Char)
+    for type,lowercase in pairs(partitionMatches) do print(" -- "..lowercase) end
+end
