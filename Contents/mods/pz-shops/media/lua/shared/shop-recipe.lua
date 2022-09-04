b9a0a6624cf48d3c695b99bb0617bd00ac030b3e
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-09-04 00:14:26 -0400

    Update shop-recipe.lua

--- Contents/mods/pz-shops/media/lua/shared/shop-recipe.lua
+++ Contents/mods/pz-shops/media/lua/shared/shop-recipe.lua
@@ -5,35 +5,14 @@ require "shop-wallet"
 --recipe Convert into Item { Authentic_MoneyStack, Result:Money, Time:30.0, }
 
 shopsAndTraders = {}
-function shopsAndTraders.OnAuthZMoneyStack(items, result, player)
-    return false
-    --[[
-    local moneyItem
-    for i=0, items:size()-1 do
-        local item = items:get(i)
-        if item and isMoneyType(item:getFullType()) and item:getModData().value then
-            moneyItem = item
-            break
-        end
-    end
-    if moneyItem and isMoneyType(result:getFullType()) then
-        generateMoneyValue(result, moneyItem:getModData().value, true)
-    end
-    --]]
-end
+function shopsAndTraders.OnAuthZMoneyStack(items, result, player) return false end
 
 local function recipeOverride()
     local allRecipes = getAllRecipes()
-
     for i=0, allRecipes:size()-1 do
         ---@type Recipe
         local recipe = allRecipes:get(i)
-
-        if recipe and isMoneyType(recipe:getResult():getFullType()) then
-            --setLuaCreate
-            recipe:setLuaTest("shopsAndTraders.OnAuthZMoneyStack")
-            print("LUA CREATE APPLIED")
-        end
+        if recipe and isMoneyType(recipe:getResult():getFullType()) then recipe:setLuaTest("shopsAndTraders.OnAuthZMoneyStack") end
     end
 end
 Events.OnGameBoot.Add(recipeOverride)
