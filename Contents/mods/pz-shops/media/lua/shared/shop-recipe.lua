811a5d51878f5de734e22940e39876eb18b69c9f
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-03-10 18:01:53 -0500

    Correct authenticZ behavior

--- Contents/mods/pz-shops/media/lua/shared/shop-recipe.lua
+++ Contents/mods/pz-shops/media/lua/shared/shop-recipe.lua
@@ -4,17 +4,19 @@ require "shop-wallet"
 --recipe Make a Stack of Money { Money, Result:Authentic_MoneyStack, Time:30.0, }
 --recipe Convert into Item { Authentic_MoneyStack, Result:Money, Time:30.0, }
 
-local shopsAndTraders = {}
-function shopsAndTraders.OnAuthZMoneyStack(items, result, player) return false end
+function ShopsAndTradersOnAuthZMoneyStack(items, result, player) return false end
 
 local function recipeOverride()
     local allRecipes = getAllRecipes()
     for i=0, allRecipes:size()-1 do
         ---@type Recipe
         local recipe = allRecipes:get(i)
-        if recipe and recipe:getResult():getFullType()=="AuthenticZClothing.Authentic_MoneyStack" then recipe:setLuaTest("shopsAndTraders.OnAuthZMoneyStack") end
+        if recipe then
+            if recipe:getResult():getFullType()=="AuthenticZClothing.Authentic_MoneyStack" then
+                recipe:setLuaTest("ShopsAndTradersOnAuthZMoneyStack")
+                recipe:setIsHidden(true)
+            end
+        end
     end
 end
 Events.OnGameBoot.Add(recipeOverride)
-
-return shopsAndTraders
