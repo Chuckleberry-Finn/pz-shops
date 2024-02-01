417157fe45d2d3161de23e5c499ff1ffc54d64e0
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-31 20:00:56 -0500

    Update shop-globalModDataClient.lua

--- Contents/mods/pz-shops/media/lua/client/shop-globalModDataClient.lua
+++ Contents/mods/pz-shops/media/lua/client/shop-globalModDataClient.lua
@@ -1,6 +1,5 @@
 ---Credit to Konijima (Konijima#9279) for clearing up networking :thumbsup:
 require "shop-commandsServerToClient"
-local _internal = require "shop-shared"
 
 CLIENT_STORES = {}
 CLIENT_WALLETS = {}
@@ -8,33 +7,10 @@ CLIENT_WALLETS = {}
 
 local function initGlobalModData(isNewGame)
 
-    --if isClient() then
-        --if ModData.exists("STORES") then ModData.remove("STORES") end
-        --if ModData.exists("WALLETS") then ModData.remove("WALLETS") end
-    --end
-
-    --CLIENT_STORES = ModData.getOrCreate("STORES")
-    --CLIENT_WALLETS = ModData.getOrCreate("WALLETS")
-
     if not isClient() then
         CLIENT_STORES = ModData.getOrCreate("STORES")
         CLIENT_WALLETS = ModData.getOrCreate("WALLETS")
     end
 
-    --triggerEvent("SHOPPING_ClientModDataReady")
 end
 Events.OnInitGlobalModData.Add(initGlobalModData)
-
-
---[[
----@param name string
----@param data table
-local function receiveGlobalModData(name, data)
-    --if name == "STORES" then
-    --    _internal.copyAgainst(CLIENT_STORES,data)
-    --elseif name == "WALLETS" then
-    --    _internal.copyAgainst(CLIENT_WALLETS,data)
-    --end
-end
-Events.OnReceiveGlobalModData.Add(receiveGlobalModData)
---]]
